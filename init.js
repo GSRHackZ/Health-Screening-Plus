6f788eb74860752d25cd8905a3bfc97e0121e907
Geahad Haymor <79179978+GSRHackZ@users.noreply.github.com>
2021-09-29 23:05:16 -0400

    Update init.js

--- init.js
+++ init.js
@@ -1,8 +1,25 @@
 const gen_ = document.getElementById("gen_");
 const nom = document.getElementById("nom");
+const splash = document.getElementsByClassName("splash")[0];
+const body = document.body;
+const html = document.getElementsByTagName("html")[0]
 
 gen_.addEventListener("click",function(){
     if(nom.value.trim().length>0){
         window.open(`screening_gen.html?name=${btoa(nom.value)}`)
     }
 })
+
+
+function start(wait){
+    setTimeout(function(){
+        splash.classList.remove("fade");
+        splash.classList.add("fadeout");
+        body.classList.add("fade");
+        body.classList.remove("lock");
+        html.classList.remove("lock");
+        splash.remove();
+    },wait)
+}
+
+start(3000)
